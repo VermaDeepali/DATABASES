810470adba825110192614bd16e995427fb44998
Deepali Verma <41052882+VermaDeepali@users.noreply.github.com>
2024-02-09 00:36:25 +0530

    Added Many queries for update, create and delete
    Can add multiple create, update and delete data

--- MongoDB/mongoDB.js
+++ MongoDB/mongoDB.js
@@ -19,16 +19,34 @@ async function main() {
                 summary: "A charming loft in Paris",
                 bedrooms: 1,
                 bathrooms: 1
-            },
-            // {
-            //     name: 'Infinite Views',
-            //     summary: 'Modern home with infinite views from the infinity pool',
-            //     property_type: 'House',
-            //     bedrooms: 5,
-            //     bathrooms: 4.5,
-            //     beds: 5
-            // }
+            }
         );
+
+        // Create Multiple Document
+        await createMultipleListings(client, [
+            {
+                name: "Infinite Views",
+                summary: "Modern home with infinite views from the infinity pool",
+                property_type: "House",
+                bedrooms: 5,
+                bathrooms: 4.5,
+                beds: 5
+            },
+            {
+                name: "Private room in London",
+                property_type: "Apartment",
+                bedrooms: 1,
+                bathroom: 1
+            },
+            {
+                name: "Beautiful Beach House",
+                summary: "Enjoy relaxed beach living in this house with a private beach",
+                bedrooms: 4,
+                bathrooms: 2.5,
+                beds: 7,
+                last_review: new Date()
+            }
+        ]);
 
         // Read One Document
         await findOneListingByName(client, "Infinite Views");
@@ -36,8 +54,14 @@ async function main() {
         // Update One Document
         await updateListingByName(client, "Infinite Views", { bedrooms: 6, beds: 8 });
 
+        // Update Many Document
+        await updateAllListingsToHavePropertyType(client);
+
         // Delete One Document
-        await deleteListingByName(client, "Lovely Loft");
+        // await deleteListingByName(client, "Lovely Loft");
+
+        // Delete Many Document
+        // await deleteListingsScrapedBeforeDate(client, new Date("2019-02-15"));
 
 
     } catch (e) {
@@ -63,6 +87,14 @@ async function createListing(client, newListing) {
     console.log(`New listing created with the following id: ${result.insertedId}`);
 }
 
+// CREATE MULTIPLE DATABASE
+async function createMultipleListings(client, newListings) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertMany(newListings);
+
+    console.log(`${result.insertedCount} new listing(s) created with the following id(s):`);
+    console.log(result.insertedIds);
+}
+
 // READ DATABASE
 async function findOneListingByName(client, nameOfListing) {
     const result = await client.db("sample_airbnb").collection("listingsAndReviews").findOne({ name: nameOfListing });
@@ -75,7 +107,7 @@ async function findOneListingByName(client, nameOfListing) {
     }
 }
 
-// UPDATE DATABASE
+// UPDATE ONE DATABASE
 async function updateListingByName(client, nameOfListing, updatedListing) {
     const result = await client.db("sample_airbnb").collection("listingsAndReviews")
         .updateOne({ name: nameOfListing }, { $set: updatedListing });
@@ -84,10 +116,25 @@ async function updateListingByName(client, nameOfListing, updatedListing) {
     console.log(`${result.modifiedCount} document(s) was/were updated.`);
 }
 
+// UPDATE MANY DATABASE
+async function updateAllListingsToHavePropertyType(client) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews")
+        .updateMany({ property_type: { $exists: false } },
+            { $set: { property_type: "Unknown" } });
+    console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+    console.log(`${result.modifiedCount} document(s) was/were updated.`);
+}
+
 // DELETE DATABASE
 async function deleteListingByName(client, nameOfListing) {
     const result = await client.db("sample_airbnb").collection("listingsAndReviews")
-            .deleteOne({ name: nameOfListing });
+        .deleteOne({ name: nameOfListing });
     console.log(`${result.deletedCount} document(s) was/were deleted.`);
 }
 
+// DELETE MANY DATABASE
+async function deleteListingsScrapedBeforeDate(client, date) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews")
+        .deleteMany({ "last_scraped": { $lt: date } });
+    console.log(`${result.deletedCount} document(s) was/were deleted.`);
+}

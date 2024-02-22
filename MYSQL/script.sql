c979754a0bcd7058a87509760cee0e3b6d27bc73
Deepali Verma <41052882+VermaDeepali@users.noreply.github.com>
2024-02-22 23:48:14 +0530

    updated sql query script and queries text

--- MYSQL/script.sql
+++ MYSQL/script.sql
@@ -20,3 +20,25 @@ CREATE TABLE `local`.`customers` (
   `PostalCode` VARCHAR(45) NULL,
   `Country` VARCHAR(45) NULL,
   PRIMARY KEY (`CustomerID`));
+
+
+-- Create Orders table
+
+CREATE TABLE `local`.`Orders` (
+  `OrderID` INT NOT NULL AUTO_INCREMENT,
+  `CustomerID` INT NULL,
+  `EmployeeID` INT NULL,
+  `OrderDate` DATETIME NULL,
+  `ShipperID` INT NULL,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  UNIQUE INDEX `OrderID_UNIQUE` (`OrderID` ASC) VISIBLE,
+  PRIMARY KEY (`OrderID`));
+
+
+-- Insert multiple values
+INSERT INTO `local`.`Orders` (CustomerID, EmployeeID, OrderDate, ShipperID, created_at, updated_at)
+VALUES
+(1, 101, '2024-02-22', 501, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(2, 102, '2024-02-23', 502, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(3, 103, '2024-02-24', 503, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);

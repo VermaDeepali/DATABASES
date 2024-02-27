bbdfd6f7fe01943c4d89361a4ff1b82c88646fe7
Deepali Verma <41052882+VermaDeepali@users.noreply.github.com>
2024-02-27 23:27:50 +0530

    Added dynamodb database queries
    Added dynamoDB schema
    Setup dynamodb local connection

---
+++ DynamoDB/dynamodb.js
@@ -0,0 +1,24 @@
+const DynamoDBClient = require("@aws-sdk/client-dynamodb");
+const { REGION, ENV, DYNAMODB_ENDPOINT } = process.env;
+
+const LOCAL_DYNAMODB_CONFIG = {
+    endpoint: DYNAMODB_ENDPOINT,
+    credentials: {
+        accessKeyId: ACCESS_KEY_ID,
+        secretAccessKey: SECRET_ACCESS_KEY,
+    },
+};
+
+
+export async function createDocumentClient() {
+    const commonOptions = {
+        region: REGION,
+    };
+
+    const options =
+    ENV === "local"
+            ? { ...commonOptions, ...LOCAL_DYNAMODB_CONFIG }
+            : commonOptions;
+    return new DynamoDBClient(options);
+}
+
